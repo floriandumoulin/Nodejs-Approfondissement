2804927c7f55e2a9e7c08ab5c4fa8bdf676321fe
floriandumoulin <florian@hbjo-online.com>
2024-01-17 16:16:46 +0100

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,12 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
+      instances: 3, 
+      exec_mode: 'cluster',
+      max_memory_restart: '200M',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log', 
+      merge_logs: true,
     },
   ],
 };
